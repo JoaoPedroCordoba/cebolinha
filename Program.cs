7405673a65fdda3b3a4669311f43e43577648060
JoaoPedroCordoba <jp.8pedro86@gmail.com.br>
2022-03-15 20:31:03 -0300

    finalizado exercicio

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// string onde vc classifica uma variavel
+string frase;
+
+Console.WriteLine("Por favor, escreva uma frase");
+frase = Console.ReadLine()!;
+
+
+string novafrase = frase.Replace("r", "l"!);
+
+Console.Write(novafrase);
+
+
+
