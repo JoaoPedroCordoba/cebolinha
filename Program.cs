3e1bc35c67a0adcee27f81086d930d3b2ff5d685
JoaoPedroCordoba <jp.8pedro86@gmail.com>
2022-03-18 20:11:57 -0300

    finalizadoexercicio

--- Program.cs
+++ Program.cs
@@ -5,9 +5,11 @@ Console.WriteLine("Por favor, escreva uma frase");
 frase = Console.ReadLine()!;
 
 
-string novafrase = frase.Replace("r", "l"!);
+string novafrase = frase
+.Replace("r", "l"!)
+.Replace("R", "L"!);
 
-Console.Write(novafrase);
+Console.WriteLine(novafrase);
 
 
 
